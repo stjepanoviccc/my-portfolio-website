bf31b0b8bc03537a6a272ae56402c13f07d9d773
stjepanoviccc <stjepanoviccc2021@gmail.com>
2024-03-14 19:29:19 +0100

    small fixes with links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import useCheckDevice from '../custom-hooks/CheckDevice'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faClose } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons'
 import Wrap from './Wrap'
 import styles from "../scss/components/Nav.module.scss";
@@ -31,19 +31,19 @@ const Navbar = () => {
             <a onClick={() => windowWidth < 1280 && toggleNavbar} href="/" className={`${windowWidth < 1280 ? styles.myLinkSmall : styles.myLinkDesktop}`} style={{ fontSize: '1.5rem' }}>AS PORTFOLIO</a>
             {windowWidth < 1280 && (
                 <button type="button" onClick={toggleNavbar}>
-                    <FontAwesomeIcon icon={faBars} className={styles.burgerMenu} />
+                    <FontAwesomeIcon icon={isActive ? faClose : faBars} className={styles.burgerMenu} />
                 </button>
             )}
             <nav className={`${styles.nav} ${windowWidth >= 1280 ?
                 styles.navDesktop :
                 `${styles.navSmall} ${!isActive ? styles.navInactive : styles.navActive}`}`}>
-                <a onClick={() => windowWidth < 1280 && toggleNavbar} href="/" className={`${windowWidth < 1280 ? styles.myLinkSmall : styles.myLinkDesktop}`}>HOME</a>
-                <a onClick={() => windowWidth < 1280 && toggleNavbar} href="#projects" className={`${windowWidth < 1280 ? styles.myLinkSmall : styles.myLinkDesktop}`}>PROJECTS</a>
-                <a onClick={() => windowWidth < 1280 && toggleNavbar} href="#contact" className={`${windowWidth < 1280 ? styles.myLinkSmall : styles.myLinkDesktop}`}>CONTACT</a>
+                <a onClick={() => windowWidth < 1280 && toggleNavbar()} href="/" className={`${windowWidth < 1280 ? styles.myLinkSmall : styles.myLinkDesktop}`}>HOME</a>
+                <a onClick={() => windowWidth < 1280 && toggleNavbar()} href="#projects" className={`${windowWidth < 1280 ? styles.myLinkSmall : styles.myLinkDesktop}`}>PROJECTS</a>
+                <a onClick={() => windowWidth < 1280 && toggleNavbar()} href="#contact" className={`${windowWidth < 1280 ? styles.myLinkSmall : styles.myLinkDesktop}`}>CONTACT</a>
                 <div className={styles.socialHolder}>
-                    <a href="https://www.linkedin.com/in/andrej-stjepanovic/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} className={styles.socialIcon} /></a>
-                    <a href="https://github.com/stjepanoviccc" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} className={styles.socialIcon} /></a>
-                    <a href="https://www.instagram.com/andrejstjepanovicc/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} className={styles.socialIcon} /></a>
+                    <a onClick={() => windowWidth < 1280 && toggleNavbar()} href="https://www.linkedin.com/in/andrej-stjepanovic/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} className={styles.socialIcon} /></a>
+                    <a onClick={() => windowWidth < 1280 && toggleNavbar()} href="https://github.com/stjepanoviccc" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} className={styles.socialIcon} /></a>
+                    <a onClick={() => windowWidth < 1280 && toggleNavbar()} href="https://www.instagram.com/andrejstjepanovicc/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} className={styles.socialIcon} /></a>
                 </div>
             </nav>
         </Wrap>
