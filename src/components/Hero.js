2c565a71a57c5dc8003de388a2e667ab87ee4d3e
stjepanoviccc <stjepanoviccc2021@gmail.com>
2024-05-20 14:32:51 +0200

    Card object fit fixed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,7 @@ import btnStyles from "../scss/components/Button.module.scss";
 const Hero = () => {
     return (
         <Wrap extendClassName={styles.wrapExtendForHero}>
-            <h3 className={styles.h3}>Hi! My name is <span className={styles.important}>Andrej Stjepanović</span></h3>
+            <h3 className={styles.h3}>Hi! My name is <span className={styles.important}>Andrej</span></h3>
             <h1 className={styles.h1}>And I'm <span className={styles.important}>Software Developer</span></h1>
             <h4 className={styles.h4}>My goal is to complete my studies in Software Engineering while simultaneously working as a Freelance Software Developer</h4>
             <div className={styles.buttonHolder}>
