29e84210d2450cdd3f3006de15bd3018553a4a18
Andrej Stjepanovic <stjepanoviccc2021@gmail.com>
2025-07-27 15:55:56 +0200

    fix links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,13 +11,13 @@ const Projects = () => {
                 <h2 id="projects" className={headerStyles.h2}>My Projects</h2>
             </div>
             <div className={styles.projectsHolder}>
-                <Card img="/projects-imomment.jpg" title="Imomment QR" desc="Digital albums with QR code feature involved and multiple panels for each user" technologies="NEXTJS TAILWINDCSS NESTJS GRAPHQL POSTGRESQL DOCKER" href="https://imomment.com/en"/>
-                <Card img="/projects-gsp-shop.jpg" title="Gastro Servis Plus Shop" desc="Custom web shop made for client in nextjs and springboot" technologies="NEXTJS TAILWINDCSS SPRINGBOOT POSTGRESQL DOCKER" href="https://gastroservisplus.ba/"/>
+                <Card img="/projects-imomment.jpg" title="Imomment QR" desc="Digital albums with QR code feature involved and multiple panels for each user" technologies="NEXTJS TAILWINDCSS NESTJS GRAPHQL POSTGRESQL DOCKER" href="imomment.com/en"/>
+                <Card img="/projects-gsp-shop.jpg" title="Gastro Servis Plus Shop" desc="Custom web shop made for client in nextjs and springboot" technologies="NEXTJS TAILWINDCSS SPRINGBOOT POSTGRESQL DOCKER" href="gastroservisplus.ba/"/>
                 <Card img="/projects-fitness.svg" title="Sitpass" desc="Fitness web app with many features involved (uni fullstack project)" technologies="ANGULAR TAILWINDCSS SPRINGBOOT HIBERNATE POSTGRESQL DOCKER JUNIT JASMINE" href="github.com/stjepanoviccc/sitpass"/>
                 <Card img="/projects-hospital-event.jpg" title="Hospital Event Scheduling" desc="Hospital event scheduling system - MERN stack" technologies="MONGODB EXPRESSJS REACT NODE DOCKER TAILWINDCSS" href="github.com/stjepanoviccc/hospital-event-scheduling"/>
                 <Card img="/projects-ems-api.svg" title="EMS API" desc="Employee Management System API" technologies="SPRINGBOOT POSTGRESQL HIBERNATE DOCKER JUNIT MOCKITO MOCKMVC LIQUIBASE RABBITMQ SWAGGER" href="github.com/stjepanoviccc/employee-management-system"/>
-                <Card img="/projects-ccs-api.svg" title="CCS API" desc="Centralized Configuration Service API" technologies="GOLANG CONSUL PROMETHEUS DOCKER SWAGGER JAEGER" href="https://github.com/stjepanoviccc/centralized-configuration-service"/>
-                <Card img="/projects-plazmica.jpg" title="Plazmica" desc="Custom restaurant website made with plain html and css for client" technologies="HTML CSS JAVASCRIPT" href="https://plazmica.com/"/>
+                <Card img="/projects-ccs-api.svg" title="CCS API" desc="Centralized Configuration Service API" technologies="GOLANG CONSUL PROMETHEUS DOCKER SWAGGER JAEGER" href="github.com/stjepanoviccc/centralized-configuration-service"/>
+                <Card img="/projects-plazmica.jpg" title="Plazmica" desc="Custom restaurant website made with plain html and css for client" technologies="HTML CSS JAVASCRIPT" href="plazmica.com">
                 <Card img="/projects-sticko.svg" title="Šticko Plast" desc="Website for sales and installation of PVC and Aluminum joinery" technologies="NEXTJS TAILWINDCSS" href="sticko-plast.com"/>
                 <Card img="/projects-organic.svg" title="Organic Shop" desc="Astra Theme(WordPress) template for Organic Shop made in React" technologies="REACT SASS FIREBASE" href="organic-shop-react.netlify.app/"/>
             </div>
